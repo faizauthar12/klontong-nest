fdff5e0f76fcbe78bdcb90d5ea8b343b0447ed4a
Faiz Authar <faizauthar@gmail.com>
2025-03-26 21:20:10 +0700

    klontong: Implement swagger OAP 3.0

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,11 +1,17 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCategoryDto {
   @IsNotEmpty({ message: 'Name is required' })
   @IsString()
+  @ApiProperty({ example: 'Snacks', description: 'Category Name' })
   name: string;
 
   @IsNotEmpty({ message: 'Description is required' })
   @IsString()
+  @ApiProperty({
+    example: 'Snacks category',
+    description: 'Category Description',
+  })
   description: string;
 }
--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,39 +1,55 @@
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateProductDto {
   @IsNotEmpty({ message: 'Name is required' })
   @IsString()
+  @ApiProperty({ example: 'Ciki', description: 'Product Name' })
   name: string;
 
   @IsNotEmpty({ message: 'Category id is required' })
   @IsNumber()
+  @ApiProperty({ example: 1, description: 'Category ID' })
   categoryId: number;
 
   @IsNotEmpty({ message: 'Description is required' })
   @IsString()
+  @ApiProperty({
+    example: 'Ciki is a snack',
+    description: 'Product Description',
+  })
   description: string;
 
   @IsNotEmpty({ message: 'Weight is required' })
   @IsNumber()
+  @ApiProperty({ example: 100, description: 'Product Weight' })
   weight: number;
 
   @IsNotEmpty({ message: 'Width is required' })
   @IsNumber()
+  @ApiProperty({ example: 10, description: 'Product Width' })
   width: number;
 
   @IsNotEmpty({ message: 'Length is required' })
   @IsNumber()
+  @ApiProperty({ example: 10, description: 'Product Length' })
   length: number;
 
   @IsNotEmpty({ message: 'Height is required' })
   @IsNumber()
+  @ApiProperty({ example: 10, description: 'Product Height' })
   height: number;
 
   @IsNotEmpty({ message: 'Image is required' })
   @IsNumber()
+  @ApiProperty({
+    example: 'http://example.com/image.jpg',
+    description: 'Product Image',
+  })
   image: string;
 
   @IsNotEmpty({ message: 'Price is required' })
   @IsNumber()
+  @ApiProperty({ example: 10000, description: 'Product Price' })
   price: number;
 }
